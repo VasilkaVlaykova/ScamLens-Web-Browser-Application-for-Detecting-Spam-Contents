17fbeaefdd97b850da41feb41ee26d5afd613fd6
Vasilka Vlaykova <vasilkavlaykova@yahoo.com>
2026-07-23 23:06:42 +0100

    Combining email and url model in one file to exceed the app loading speed

--- app/pages/1_Home.py
+++ app/pages/1_Home.py
@@ -51,7 +51,7 @@ st.write('A detection platform designed to analyse text messages, emails, and UR
 # Using HTML to create a custom warning box to provides to the user.
 st.markdown(
     """
-    <div style='background-color: #fbc02d; color: black; padding: 1rem; border-radius: 0.5rem; font-size: 0.85rem; margin-bottom: 0.5rem;'>
+    <div style='background-color: #fdd835; color: black; padding: 1rem; border-radius: 0.5rem; font-size: 0.85rem; margin-bottom: 0.5rem;'>
     ⚠️ Please do not input any sensitive information such as name, address, phone number, or your personal email address. The platform will analyse your content without storing any data.
     </div>
     """,
@@ -62,24 +62,13 @@ st.markdown(
 # Streamlit function to load global only one time and run the Email and URL models and TF-IDF vectorizer models.
 
 @st.cache_resource(show_spinner=False)
-def load_models():
-    email_model = joblib.load("models/email_svm_model.pkl")
-    email_vectoriser = joblib.load("models/email_tfidf.pkl")
-    url_model = joblib.load("models/url_svm_model.pkl")
-    url_vectorizer = joblib.load("models/url_tfidf.pkl")
-
-    return (
-        email_model,
-        email_vectoriser,
-        url_model,
-        url_vectorizer
-    )
-
-email_model, email_vectoriser, url_model, url_vectorizer = load_models()
+def email_load_models():
+    return joblib.load('models/email_pipeline.pkl')
 
 
-
-
+@st.cache_resource(show_spinner=False)
+def url_load_model():
+    return joblib.load('models/url_pipeline.pkl')
 
 
 
@@ -88,11 +77,11 @@ email_model, email_vectoriser, url_model, url_vectorizer = load_models()
 #Used for both SMS/email and URL analysis to avoid repeating the same
 #predict, probability, display logic twice.
 
-def predict_and_display(model, vectorizer, text, spam_label='Spam', ham_label='Legitimate', show_chart=False):
+def predict_and_display(pipeline, text, spam_label='Spam', ham_label='Legitimate', show_chart=False):
 
-    vector = vectorizer.transform([text])
-    prediction = model.predict(vector)[0]
-    probability = model.predict_proba(vector)[0]
+    #vector = vectorizer.transform([text])
+    prediction = pipeline.predict([text])[0]
+    probability = pipeline.predict_proba([text])[0]
 
     ham_probability = probability[0] * 100
     spam_probability = probability[1] * 100
@@ -106,7 +95,7 @@ def predict_and_display(model, vectorizer, text, spam_label='Spam', ham_label='L
         st.write(f"Prediction: **{ham_label}**")
         st.write(f"Ham probability: **{ham_probability:.2f}%**")
         if show_chart:
-             st.plotly_chart(chart(ham_probability, 'Ham Probability'))
+          st.plotly_chart(chart(ham_probability, 'Ham Probability'))
              
     return prediction, spam_probability, ham_probability    
 
@@ -114,13 +103,9 @@ def predict_and_display(model, vectorizer, text, spam_label='Spam', ham_label='L
 
 #Creating 3 tabs SMS, Email, URL, user to has a option to choose what type of service he wants to use.
 tab1, tab2, tab3 = st.tabs(['SMS', 'Email','URL'])
-
-
 # Creating a function to apply the plotly chart Indicator into tabs SMS, Email, and URL.
 def chart(value, label):
-    import plotly.graph_objects as go # Calling the plotly libray incide the chart for fasting loading.
-
-
+    import plotly.graph_objects as go 
     fig = go.Figure(go.Indicator(
         mode='gauge+number',
         value = value,
@@ -143,28 +128,27 @@ with tab1:
     st.subheader('SMS')
 
 # Using st.form to clean the text from the imput box, and reaedy to be use for another analysis of text.
-    with st.form(key='sms_form', clear_on_submit=True):
+    with st.form(key='sms_form',clear_on_submit=True):
         sms_text = st.text_area('Please enter or paste an SMS message')
-    
         analyse_Sbt = st.form_submit_button('Analyse SMS', type='primary')
 
 # Using the IF condition and HTML for custom style box to provide to the user a warning message if he dit not paste any text in the input box.
         if sms_text.strip()=="":
                
                st.markdown(
-    """
-    <div style='background-color: #fbc02d; color: black; padding: 1rem; border-radius: 0.5rem; font-size: 0.85rem; margin-bottom: 0.5rem;'>
-    ⚠️ Please enter or paste an SMS message.
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-               
+               """
+               <div style='background-color: #fff59d; color: #333333; padding: 1rem; border-radius: 0.5rem;'>
+               🔒 This system does not store your messages. Your text is deleted automatically right after it is analyzed.
+               </div>
+               """,
+               unsafe_allow_html=True
+               )          
         else:
+               email_pipeline = email_load_models()
                # calling the probability function and plotly chart to be display.
                st.header('SMS Analysis')
                sms_prediction, spam_sms_probability, ham_sms_probability = predict_and_display(
-               email_model, email_vectoriser, sms_text,
+               email_pipeline, sms_text,
                spam_label='Spam', ham_label='Legitimate', show_chart=True
                )
 
@@ -176,8 +160,9 @@ with tab1:
                if urls:
                      st.subheader("URL Analysis")
                      for url in urls:
+                      url_pipeline = url_load_model()
                       url_prediction, spam_url_probability, ham_url_probability = predict_and_display(
-                      url_model, url_vectorizer, url,
+                      url_pipeline, url,
                       spam_label='Spam', ham_label='Legitimate URL', show_chart=False
                       )
 
@@ -195,16 +180,13 @@ with tab1:
                                  # displaying as well the name of the found letters.
                                  name = unicodedata.name(i)          
                                  script = name.split()[0].capitalize() 
-                                 st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")
-                              
+                                 st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")        
                      else:
                               st.info('The link does not have any hidden mixed alphabets letter')
                      if zero: # Diplaying the zero-width to the user with a warning message.
                               st.warning('⚠️ Hidden zero-width characters found:')
                               for j in zero:
                                 st.write(f"- `{j}` → `{hex(ord(j))}`")
-                                
-
                      else:  # If the link does not have any hidden numbers will display a message to the user.
                             st.info(' The link does not have any hidden numbers')
                
@@ -222,67 +204,54 @@ with tab1:
 # For development of Tab 2 Email section is used the same code , only the name of the text box and model SVM and model TF-IDF are changed.
 with tab2:
     st.subheader('Email')
-
-
     with st.form(key='email_form', clear_on_submit=True):
         email_text = st.text_area('Please enter or paste an Email message')
-    
         analyse_Sbt = st.form_submit_button('Analyse Email', type='primary')
-
         if email_text.strip()=="":
             st.markdown(
-    """
-    <div style='background-color: #fbc02d; color: black; padding: 1rem; border-radius: 0.5rem; font-size: 0.85rem; margin-bottom: 0.5rem;'>
-    ⚠️ Please enter or paste an SMS message.
-    </div>
-    """,
-    unsafe_allow_html=True
-        )
-            
+            """
+            <div style='background-color: #fff59d; color: #333333; padding: 1rem; border-radius: 0.5rem;'>
+            🔒 This system does not store your email. Your text is deleted automatically right after it is analyzed.
+            </div>
+            """,
+            unsafe_allow_html=True
+            )      
         else:
+            email_pipeline=email_load_models()
             st.header('Email Analysis')
             email_prediction, spam_email_probability, ham_email_probability = predict_and_display(
-               email_model, email_vectoriser, email_text,
+               email_pipeline, email_text,
                spam_label='Spam', ham_label='Legitimate', show_chart=True
                )
-
-
+            
             urls = re.findall(r'(https?://[^\s]+|www\.[^\s]+)', email_text)
-
             if urls:
                 st.subheader("URL Analysis")
                 for url in urls:
-                    
+                    url_pipeline=url_load_model()
                     url_prediction, spam_url_probability, ham_url_probability = predict_and_display(
-                    url_model, url_vectorizer, url,
+                    url_pipeline, url,
                     spam_label='Spam', ham_label='Legitimate URL', show_chart=False
                     )
-
                     st.write(f"URL: {url}")
                     mix_char = r"[\u0370-\u03FF\u0400-\u04FF\u0530-\u058F]"
                     zero_width = r"[\u200B\u200C\u200D\u2060]"
                     hidden = re.findall(mix_char,url)
                     zero = re.findall(zero_width, url)
                     if hidden:
-                           st.warning('⚠️ The link has mixed alphabet characters:')
-                           for i in hidden:
-                                 
-                                 name = unicodedata.name(i)          
-                                 script = name.split()[0].capitalize() 
-                                 st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")
-                              
+                        st.warning('⚠️ The link has mixed alphabet characters:')
+                        for i in hidden:      
+                            name = unicodedata.name(i)          
+                            script = name.split()[0].capitalize() 
+                            st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")     
                     else:
-                              st.info('The link does not have any hidden mixed alphabets letter')
+                        st.info('The link does not have any hidden mixed alphabets letter')
                     if zero:
-                              st.warning('⚠️ Hidden zero-width characters found:')
-                              for j in zero:
-                                st.write(f"- `{j}` → `{hex(ord(j))}`")
-                                
-
+                        st.warning('⚠️ Hidden zero-width characters found:')
+                        for j in zero:
+                            st.write(f"- `{j}` → `{hex(ord(j))}`")
                     else:
-                            st.info(' The link does not have any hidden numbers')
-
-
+                         st.info(' The link does not have any hidden numbers')
             else:
                 st.info("No URLs found in this Email.")
                 if sms_prediction == 1:
@@ -294,31 +263,27 @@ with tab2:
 # For tab3 the URL section was used the same code only was removed the email model and TF-IDF model representation of the code.
 with tab3:
     st.subheader('URL link')
- 
     with st.form(key='url_form', clear_on_submit=True):
         url_text = st.text_area('Please enter or paste an URL link')
     
         analyse_Sbt = st.form_submit_button('Analyse URL link', type='primary')
-
         if url_text.strip()=="":
             st.markdown(
-    """
-    <div style='background-color: #fbc02d; color: black; padding: 1rem; border-radius: 0.5rem; font-size: 0.85rem; margin-bottom: 0.5rem;'>
-    ⚠️ Please enter or paste an URL link.
-    </div>
-    """,
-    unsafe_allow_html=True
-        )
+            """
+            <div style='background-color: #fff59d; color: #333333; padding: 1rem; border-radius: 0.5rem;'>
+            🔒 This system does not store your URL link. Your text is deleted automatically right after it is analyzed.
+            </div>
+            """,
+            unsafe_allow_html=True
+            )
         else:
             urls = re.findall(r'(https?://[^\s]+|www\.[^\s]+)', url_text)
-
             if urls:
                  for url in urls:
-
-                
+                  url_pipeline=url_load_model()
                   st.header(' URL link Analysis')
                   url_prediction, spam_url_probability, ham_url_probability = predict_and_display(
-                      url_model, url_vectorizer, url,
+                      url_pipeline, url,
                       spam_label='Spam', ham_label='Legitimate URL', show_chart=True
                       )
                   st.write(f"URL: {urls}")
@@ -328,25 +293,18 @@ with tab3:
                   zero = re.findall(zero_width, url)
                  if hidden:
                     st.warning('⚠️ The link has mixed alphabet characters:')
-                    for i in hidden:
-                                 
+                    for i in hidden:         
                         name = unicodedata.name(i)          
                         script = name.split()[0].capitalize() 
-                        st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")
-                              
+                        st.write(f"{i} → {hex(ord(i))}→ {script} alphabet")          
                  else:
                     st.info('The link does not have any hidden mixed alphabets letter')
                  if zero:
                     st.warning('⚠️ Hidden zero-width characters found:')
                     for j in zero:
                         st.write(f"- `{j}` → `{hex(ord(j))}`")
-                                
-
                  else:
-                            st.info(' The link does not have any hidden numbers')
-                 
-
-
+                    st.info(' The link does not have any hidden numbers')
             else:
                 st.info("No URLs found in this Email.")
                 if sms_prediction == 1:
@@ -354,6 +312,8 @@ with tab3:
                        "⚠️ This Email appears to be **spam** and may contain **hidden malicious links** "
                        "behind buttons or words. Do **NOT** click anywhere in the original email."
                        )
+email_load_models()
+url_load_model()
             
 
 
